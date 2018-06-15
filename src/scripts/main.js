e4c0e7aac260609a3e4e70354d0ffa3d27353420
Philip Patton <ppatton35745@gmail.com>
2018-06-15 11:49:43 -0500

    attempt to fix function

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,63 +1,41 @@
-/*
-    In the following code, replace `event` with the correct string to handle
-    a click event on the button. Then write the function that has instructions
-    for activating the corresponding power. The function should...
-        1. Remove the `disabled` class
-        2. Add the `enabled` class
+const DragDropManager = Object.create(null, {
+    init: {
+        value: () => {
+            const stages = document.querySelectorAll(".stage")
 
-*/
-
-const activateFlight = (e) => {
-    document.querySelector("#flight").classList.toggle("enabled");
-    document.querySelector("#flight").classList.toggle("disabled");
-}
-
-const activateMindreading = (e) => {
-    document.querySelector("#mindreading").classList.toggle("enabled");
-    document.querySelector("#mindreading").classList.toggle("disabled");
-}
-
-const activateXray = (e) => {
-    document.querySelector("#xray").classList.toggle("enabled");
-    document.querySelector("#xray").classList.toggle("disabled");
-}
-
-const activateAll = (e) => {
-    sections = document.querySelectorAll(".power") //.classList.add("enabled");
-    sections.forEach(element => {
-        element.classList.add("enabled");
-        element.classList.remove("disabled");
-    });
-}
-
-const disableAll = (e) => {
-    sections = document.querySelectorAll(".power") //.classList.add("enabled");
-    sections.forEach(element => {
-        element.classList.add("disabled");
-        element.classList.remove("enabled");
-    });
-}
-
-document.querySelector("#activate-flight").addEventListener("click", activateFlight);
-document.querySelector("#activate-mindreading").addEventListener("click", activateMindreading);
-document.querySelector("#activate-xray").addEventListener("click", activateXray);
-document.querySelector("#activate-all").addEventListener("click", activateAll);
-document.querySelector("#deactivate-all").addEventListener("click", disableAll);
+            stages.forEach(stage => {
+                // Gain reference of item being dragged
+                stage.ondragstart = e => {
+                    e.dataTransfer.setData("text", e.target.classList)
+                }
+            })
 
 
+            const targets = document.querySelectorAll(".target")
 
+            targets.forEach(target => {
+                // Dragover not supported by default. Turn that off.
+                target.ondragover = e => e.preventDefault()
 
+                target.ondrop = e => {
+                    // Enabled dropping on targets
+                    e.preventDefault()
 
+                    // Determine what's being dropped
+                    const data = e.dataTransfer.getData("text")
 
+                    // Append card to target component as child
 
-/*
-    Now write two more event handlers that activate the other two powers
-    when the corresponding buttons are clicked.
-*/
+                    if (!e.target.childNodes[0] && !e.target.classList.contains("stage")) {
+                        e.target.appendChild(document.querySelector(`.${data.split(" ")[1]}`))
+                    }
+                    // TODO: This should only happen if the target has no children nodes
+                    // TODO: This should not happen if the target is another stage card
+                    
+                }
+            })
+        }
+    }
+})
 
-
-/*
-    Write two more event handlers for activating and deactivating all powers
-    when the corresponding buttons are clicked. You will need to use the
-    `document.querySelectorAll()` method for these.
-*/
+DragDropManager.init()
